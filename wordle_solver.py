39cfe551211d1ffe1ae12235024d6260532d0145
unknown <benjaminjameskearney@gmail.com>
2022-04-25 22:37:57 -0400

    add ui

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,7 +1,7 @@
 from nltk.corpus import words
 
 import nltk
-
+import tkinter
 nltk.download('words')
 '''
 app = tkinter.Tk()
@@ -20,9 +20,19 @@ grey_letters = [char for char in input("grey letters: ")]
 possible_words = [word for word in possible_words if 1 not in [c in word for c in grey_letters]]
 
 for i in range(word_length):
-    if input(f'letter #{i+1} green? (y/n): ').lower() == 'y':
-        green = input(f'Enter {i+1}st letter: ')
+    green = input(f'Enter letter #{i+1} (if unknown, hit Enter): ')
+    if green != '':
         possible_words = [str(word) for word in possible_words if word[i] == green]
 
 
 print(possible_words)
+
+app = tkinter.Tk()
+app.title("Wordle Solver")
+scroll = tkinter.Scrollbar(app)
+listbox = tkinter.Listbox(app, height=35, width=40, yscrollcommand=scroll.set)
+for i in range(len(possible_words)):
+    listbox.insert(i, possible_words[i])
+scroll.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+listbox.pack()
+app.mainloop()
